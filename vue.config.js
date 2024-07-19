e6e4539d61e0d46007427845bbcc177f68976cab
Kanewwwwww <70993423+Kanewwwwww@users.noreply.github.com>
2024-07-19 16:53:31 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false,
+  devServer:{
+    proxy:{
+      '/api': {
+        target: 'https://localhost:15593',
+        secure: false, 
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }
+      }
+    }
+  }
 })
