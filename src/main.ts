a5e4d9690e499a9d23b44870a1134f86bc6263dc
Kanewwwwww <70993423+Kanewwwwww@users.noreply.github.com>
2024-07-19 16:53:05 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import {store, key} from './store'
+import HighchartsVue from 'highcharts-vue';
 import { KanbanPlugin  } from '@syncfusion/ej2-vue-kanban';
 
-createApp(App).use(store).use(router).use(KanbanPlugin).mount('#app');
+createApp(App).use(store,key).use(router).use(HighchartsVue).use(KanbanPlugin).mount('#app')
