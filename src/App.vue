974a8e9f3ec081a31f44b7ad5d5481d99c7281d1
Kanewwwwww <70993423+Kanewwwwww@users.noreply.github.com>
2024-10-07 11:06:45 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,20 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/kanben">Kanban</router-link>
+    <router-link to="/chart">chart</router-link> |
+    <router-link to="/kenban">kenban</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style lang="scss">
 @import 'assets/sass/main.scss';
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  position: relative;
-  height: 100vh; 
 }
 
 nav {
